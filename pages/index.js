fbc4f62de857413f337f90027ac5caa16dc2f582
Noor-Kimutai <noorkimutai@gmail.com>
2022-10-19 13:08:22 +0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Nav from "../components/Nav.jsx"
+import { useState } from 'react';
 
 export default function Home() {
-  function printHello() {
+  const [newGreeting, setNewGreeting] = useState('');
+  
+  function runTransaction() {
     console.log("Well done steak is better than medium rare")
   }
 
@@ -18,6 +22,8 @@ export default function Home() {
         <link rel="icon" href="https://i.imgur.com/hvNtbgD.png" />
       </Head>
 
+      <Nav />
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to my <a href="https://github.com/Noobsybot/beginner-emerald-dapp" target="_blank">Emerald DApp!</a>
@@ -28,8 +34,8 @@ export default function Home() {
         </p>
 
         <div className={styles.flex}>
-        <button onClick={printHello}>Hello</button>
-        <button onClick={printGoodbye}>Goodbye</button>
+        <button onClick={runTransaction}>Run Transaction</button>
+        <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
         </div>
 
       </main>
