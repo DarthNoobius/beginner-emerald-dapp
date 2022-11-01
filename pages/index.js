634878ff0d1dc2af5612617570faf27f9f5a8651
Noor-Kimutai <noorkimutai@gmail.com>
2022-11-01 13:04:34 +0300

    Updated to Chapter 4 Day 3

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
       args: (arg, t) => [] // ARGUMENTS GO IN HERE
     })
   
-    console.log("Response from our script: " + response);
+    //console.log("Response from our script: " + response);
     setGreeting(response);
   }
   useEffect(() => {
@@ -47,6 +47,59 @@ export default function Home() {
     console.log("Response from the SimpleTest script: " + response);
     setNumber(response);
   }
+  useEffect(() => {
+    numberReader()
+  }, [])
+
+  async function executeCtrlcScript() {
+    const response = await fcl.query({
+      cadence: `
+      pub fun main(
+        a: Int, 
+        b: String, 
+        c: UFix64, 
+        d: Address, 
+        e: Bool,
+        f: String?,
+        g: [Int],
+        h: {String: Address}
+      ): String {
+        // Example:
+        // a = 2
+        // b = "Jacob is so cool"
+        // c = 5.0
+        // d = 0x6c0d53c676256e8c
+        // e = true
+        // f = nil
+        // g = [1, 2, 3]
+        // h = {"FLOAT": 0x2d4c3caffbeab845, "EmeraldID": 0x39e42c67cc851cfb}
+  
+        return b
+      }
+      `,
+      args: (arg, t) => [
+        arg("56", t.Int),
+        arg("Svelte is overated!", t.String),
+        arg("3.142", t.UFix64),
+        arg("0x8e56d24cc7f80589", t.Address),
+        arg(false, t.Bool),
+        arg(null, t.Optional(t.String)),
+        arg([3, 5, 8], t.Array(t.Int)),
+        arg(
+          [
+            { key: "FLOAT", value: "0x2d4c3caffbeab845" },
+            { key: "EmeraldID", value: "0x39e42c67cc851cfb" }
+          ], 
+          t.Dictionary({ key: t.String, value: t.Address })
+        )
+      ]
+    })
+    console.log(response)
+  }
+  useEffect(() => {
+    executeCtrlcScript()
+  }, [])
+
 
   function printGoodbye() {
     console.log("Goats are pyschotic sheep!")
@@ -76,15 +129,12 @@ export default function Home() {
         <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Pizza is always the answer!" />
         </div>
 
-
         <div className={styles.flex}>
-        <button onClick={numberReader}>What's the number?</button>
+        
         </div>
-
+        
         <p> {greeting}</p>
         <p> {number}</p>
-
-        
 
       </main>
     </div>
